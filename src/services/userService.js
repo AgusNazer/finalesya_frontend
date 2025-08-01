ae01faf4c453f0b4591e8d42026dec0d5dd4c9da
agusNazer <agustin.nazer@hotmail.com>
2025-08-01 11:26:05 -0300

    api url

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,16 +2,19 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+// GET /api/users
 export const getUsers = async () => {
-  const res = await axios.get(API_URL);
+  const res = await axios.get(`${API_URL}/users`);
   return res.data;
 };
 
+// POST /api/users
 export const createUser = async (user) => {
-  const res = await axios.post(API_URL, user);
+  const res = await axios.post(`${API_URL}/users`, user);
   return res.data;
 };
 
+// DELETE /api/users/{id}
 export const deleteUser = async (id) => {
-  await axios.delete(`${API_URL}/${id}`);
+  await axios.delete(`${API_URL}/users/${id}`);
 };
